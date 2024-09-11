09b38eac0b3c521bed6d41cb0a1283083da9a0d9
Florian Wilhelm <2292245+fwilhe@users.noreply.github.com>
2024-09-11 12:38:48 +0200

    Update extra-schema.sql

--- extra-schema.sql
+++ extra-schema.sql
@@ -4,8 +4,7 @@
 
 CREATE OR REPLACE VIEW public.sourcepackagecve
  AS
- SELECT ((all_cve.cve_id || deb_cve.deb_source) || deb_cve.deb_version::text) || dist_cpe.cpe_version AS my_id,
-    all_cve.cve_id,
+ SELECT all_cve.cve_id AS cve_id,
     deb_cve.deb_source AS source_package_name,
     deb_cve.deb_version AS source_package_version,
     dist_cpe.cpe_version AS gardenlinux_version,
@@ -18,5 +17,21 @@ CREATE OR REPLACE VIEW public.sourcepackagecve
   ORDER BY all_cve.cve_id;
 
 ALTER TABLE public.sourcepackagecve
+    OWNER TO glvd;
+
+-- View: public.sourcepackage
+
+-- DROP VIEW public.sourcepackage;
+
+CREATE OR REPLACE VIEW public.sourcepackage
+ AS
+ SELECT debsrc.deb_source AS source_package_name,
+    debsrc.deb_version AS source_package_version,
+    dist_cpe.cpe_version AS gardenlinux_version
+   FROM debsrc
+     JOIN dist_cpe ON debsrc.dist_id = dist_cpe.id
+  WHERE dist_cpe.cpe_product = 'gardenlinux'::text;
+
+ALTER TABLE public.sourcepackage
     OWNER TO glvd;
 
