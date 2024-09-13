58887a8c015e97d09fb204127699fed7631abc1b
Florian Wilhelm <florian.wilhelm02@sap.com>
2024-09-13 13:37:47 +0200

    quiet

--- update-cluster.sh
+++ update-cluster.sh
@@ -2,13 +2,13 @@
 
 # very hacky, surely there is a better way to do this..
 
-podman pull --arch=amd64 ghcr.io/gardenlinux/glvd-postgres:edgefulldata
+podman pull -q --arch=amd64 ghcr.io/gardenlinux/glvd-postgres:edgefulldata
 IMAGE=$(podman inspect --format='{{index .RepoDigests 0}}' ghcr.io/gardenlinux/glvd-postgres:edgefulldata)
 kubectl set image sts/glvd-database glvd-postgres=$IMAGE
 
-podman pull --arch=amd64 ghcr.io/gardenlinux/glvd-api:edge
+podman pull -q --arch=amd64 ghcr.io/gardenlinux/glvd-api:edge
 IMAGE=$(podman inspect --format='{{index .RepoDigests 0}}' ghcr.io/gardenlinux/glvd-api:edge)
 kubectl set image deploy/glvd glvd-api=$IMAGE
 
-podman pull --arch=arm64 ghcr.io/gardenlinux/glvd-postgres:edgefulldata
-podman pull --arch=arm64 ghcr.io/gardenlinux/glvd-api:edge
+podman pull -q --arch=arm64 ghcr.io/gardenlinux/glvd-postgres:edgefulldata
+podman pull -q --arch=arm64 ghcr.io/gardenlinux/glvd-api:edge
