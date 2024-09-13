5de390053394639772779895fb4dfa62070b3103
Florian Wilhelm <florian.wilhelm02@sap.com>
2024-09-13 11:29:31 +0200

    add hacky stript to update cluster

---
+++ update-cluster.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# very hacky, surely there is a better way to do this..
+
+podman pull --arch=amd64 ghcr.io/gardenlinux/glvd-postgres:edgefulldata
+IMAGE=$(podman inspect --format='{{index .RepoDigests 0}}' ghcr.io/gardenlinux/glvd-postgres:edgefulldata)
+kubectl set image sts/glvd-database glvd-postgres=$IMAGE
+
+podman pull --arch=amd64 ghcr.io/gardenlinux/glvd-api:edge
+IMAGE=$(podman inspect --format='{{index .RepoDigests 0}}' ghcr.io/gardenlinux/glvd-api:edge)
+kubectl set image deploy/glvd glvd-api=$IMAGE
+
+podman pull --arch=arm64 ghcr.io/gardenlinux/glvd-postgres:edgefulldata
+podman pull --arch=arm64 ghcr.io/gardenlinux/glvd-api:edge
