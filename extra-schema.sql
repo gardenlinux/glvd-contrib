fd61b71f9a4d6f0df5c924556e09e0fbb6455d93
Florian Wilhelm <2292245+fwilhe@users.noreply.github.com>
2024-09-16 15:42:09 +0200

    Create VIEWs for source package - cve relationship (#10)

---
+++ extra-schema.sql
@@ -0,0 +1,55 @@
+-- View: public.sourcepackagecve
+
+-- DROP VIEW public.sourcepackagecve;
+
+CREATE OR REPLACE VIEW public.sourcepackagecve
+ AS
+ SELECT all_cve.cve_id AS cve_id,
+     CASE
+      WHEN data->'metrics'->'cvssMetricV40' IS NOT NULL THEN
+        (data->'metrics'->'cvssMetricV40'->0->'cvssData'->>'baseScore')::numeric
+      WHEN data->'metrics'->'cvssMetricV31' IS NOT NULL THEN
+        (data->'metrics'->'cvssMetricV31'->0->'cvssData'->>'baseScore')::numeric
+      WHEN data->'metrics'->'cvssMetricV30' IS NOT NULL THEN
+        (data->'metrics'->'cvssMetricV30'->0->'cvssData'->>'baseScore')::numeric
+      ELSE NULL
+    END AS base_score,
+     CASE
+      WHEN data->'metrics'->'cvssMetricV40' IS NOT NULL THEN
+        (data->'metrics'->'cvssMetricV40'->0->'cvssData'->>'vectorString')::text
+      WHEN data->'metrics'->'cvssMetricV31' IS NOT NULL THEN
+        (data->'metrics'->'cvssMetricV31'->0->'cvssData'->>'vectorString')::text
+      WHEN data->'metrics'->'cvssMetricV30' IS NOT NULL THEN
+        (data->'metrics'->'cvssMetricV30'->0->'cvssData'->>'vectorString')::text
+      ELSE NULL
+    END AS vector_string,
+    deb_cve.deb_source AS source_package_name,
+    deb_cve.deb_version AS source_package_version,
+    dist_cpe.cpe_version AS gardenlinux_version,
+    deb_cve.debsec_vulnerable AS is_vulnerable,
+    all_cve.data ->> 'published'::text AS cve_published_date
+   FROM all_cve
+     JOIN deb_cve USING (cve_id)
+     JOIN dist_cpe ON deb_cve.dist_id = dist_cpe.id
+  WHERE dist_cpe.cpe_product = 'gardenlinux'::text
+  ORDER BY all_cve.cve_id;
+
+ALTER TABLE public.sourcepackagecve
+    OWNER TO glvd;
+
+-- View: public.sourcepackage
+
+-- DROP VIEW public.sourcepackage;
+
+CREATE OR REPLACE VIEW public.sourcepackage
+ AS
+ SELECT debsrc.deb_source AS source_package_name,
+    debsrc.deb_version AS source_package_version,
+    dist_cpe.cpe_version AS gardenlinux_version
+   FROM debsrc
+     JOIN dist_cpe ON debsrc.dist_id = dist_cpe.id
+  WHERE dist_cpe.cpe_product = 'gardenlinux'::text;
+
+ALTER TABLE public.sourcepackage
+    OWNER TO glvd;
+
