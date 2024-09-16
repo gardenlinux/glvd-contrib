1e617278d893b27a41242c81d0d0891e5eca74ad
Florian Wilhelm <florian.wilhelm02@sap.com>
2024-09-16 09:24:44 +0200

    fix

--- update-cluster.sh
+++ update-cluster.sh
@@ -4,11 +4,11 @@
 
 podman pull -q --arch=amd64 ghcr.io/gardenlinux/glvd-postgres:edgefulldata
 IMAGE=$(podman inspect --format='{{index .RepoDigests 0}}' ghcr.io/gardenlinux/glvd-postgres:edgefulldata)
-kubectl set image sts/glvd-database glvd-postgres=$IMAGE
+kubectl set image sts/glvd-database glvd-postgres="$IMAGE"
 
 podman pull -q --arch=amd64 ghcr.io/gardenlinux/glvd-api:edge
 IMAGE=$(podman inspect --format='{{index .RepoDigests 0}}' ghcr.io/gardenlinux/glvd-api:edge)
-kubectl set image deploy/glvd glvd-api=$IMAGE
+kubectl set image deploy/glvd glvd-api="$IMAGE"
 
 podman pull -q --arch=arm64 ghcr.io/gardenlinux/glvd-postgres:edgefulldata
 podman pull -q --arch=arm64 ghcr.io/gardenlinux/glvd-api:edge
