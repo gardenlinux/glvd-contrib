9f870af052fb457e624ffe02fb1dad118ca35f2e
Florian Wilhelm <2292245+fwilhe@users.noreply.github.com>
2024-09-18 14:05:44 +0200

    Update pg-init-entrypoint.sh

--- pg-init-entrypoint.sh
+++ pg-init-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-set -euo pipefail
 
 echo "$PGHOST:$PGPORT:$PGDATABASE:$PGUSER:$PGPASSWORD" > ~/.pgpass
 chmod 0600 ~/.pgpass
 
-dropdb glvd || true
+dropdb glvd
+createdb glvd
 psql glvd -f /glvd.sql
