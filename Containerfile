e8beff13270a01eaef157d845a17253f06a06b35
Florian Wilhelm <florian.wilhelm02@sap.com>
2024-10-15 14:14:04 +0200

    delete obsolete/moved files

--- Containerfile
+++
@@ -1,3 +0,0 @@
-FROM ghcr.io/gardenlinux/glvd-postgres:edgenotls
-
-COPY glvd.sql /docker-entrypoint-initdb.d/glvd.sql
--- pg-init-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "$PGHOST:$PGPORT:$PGDATABASE:$PGUSER:$PGPASSWORD" > ~/.pgpass
-chmod 0600 ~/.pgpass
-
-dropdb glvd
-createdb glvd
-psql glvd -f /glvd.sql
