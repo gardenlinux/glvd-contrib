ad9e836bfacb985b7e3cece5651991e2f8b91dfe
Florian Wilhelm <florian.wilhelm02@sap.com>
2024-12-10 13:18:42 +0100

    finish poc for release notes helper

--- release-notes-helper.py
+++ release-notes-helper.py
@@ -1,33 +1,54 @@
 import requests
 
-
 # just a proof of concept, don't judge the code
 
 version = 'today'
 previous_version = '1592.4'
 
-
-
 url = f"https://glvd.ingress.glvd.gardnlinux.shoot.canary.k8s-hana.ondemand.com/v1/cves/{version}"
-cves_new = requests.get(url)
-foo = cves_new.json()
+cves_new = requests.get(url).json()
 
 url_previous = f"https://glvd.ingress.glvd.gardnlinux.shoot.canary.k8s-hana.ondemand.com/v1/cves/{previous_version}"
-cves_old = requests.get(url_previous)
-bar = cves_old.json()
+cves_old = requests.get(url_previous).json()
 
-
-list_cves_new = [{'cveId': x['cveId'], 'sourcePackageName': x['sourcePackageName'], 'sourcePackageVersion': x['sourcePackageVersion']} for x in foo]
-list_cves_old = [{'cveId': x['cveId'], 'sourcePackageName': x['sourcePackageName'], 'sourcePackageVersion': x['sourcePackageVersion']} for x in bar]
-
-
+list_cves_new = [{'cveId': cve['cveId'], 'sourcePackageName': cve['sourcePackageName'], 'sourcePackageVersion': cve['sourcePackageVersion']} for cve in cves_new]
+list_cves_old = [{'cveId': cve['cveId'], 'sourcePackageName': cve['sourcePackageName'], 'sourcePackageVersion': cve['sourcePackageVersion']} for cve in cves_old]
 
 diff = list(set([x['cveId'] for x in list_cves_old]) - set([x['cveId'] for x in list_cves_new]))
 
-print(diff)
+# print(diff)
+
+
+url = f"https://glvd.ingress.glvd.gardnlinux.shoot.canary.k8s-hana.ondemand.com/v1/distro/{version}"
+
+new_version_numbers = requests.get(url).json()
+
+
+
+packages = []
+packages_to_fix_mapping = {}
 
 for cve in diff:
-    for x in list_cves_old:
-        if x['cveId'] == cve:
-            print(x)
+    for package_name in list_cves_old:
+        if package_name['cveId'] == cve:
+            # print(package_name)
+            pkg = package_name['sourcePackageName']
+            packages.append(pkg)
+            # print(pkg)
+            if pkg in packages_to_fix_mapping:
+                packages_to_fix_mapping[pkg]['fixes'].append(cve)
+            else:
+                for v in new_version_numbers:
+                    if pkg == v['sourcePackageName']:
+                        packages_to_fix_mapping[pkg] = {'fixes': [cve], 'version': v['sourcePackageVersion']}
 
+packages = set(sorted(packages))
+# print(packages)
+# print(packages_to_fix_mapping)
+
+print('The following packages have been upgraded, to address the mentioned CVEs:')
+for package_name in packages_to_fix_mapping.keys():
+    package = packages_to_fix_mapping[package_name]
+    print(f"- upgrade '{package_name}' to `{package['version']}`")
+    for cve in package['fixes']:
+        print(f'  - {cve}')
