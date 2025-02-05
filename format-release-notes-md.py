54e1a27c7d13a54e5183eb0876fa16fa65df9bb3
Florian Wilhelm <florian.wilhelm02@sap.com>
2025-02-05 10:51:47 +0100

    Add script to format patch release notes as markdown
    See https://github.com/gardenlinux/glvd-api/pull/81

---
+++ format-release-notes-md.py
@@ -0,0 +1,50 @@
+import argparse
+import requests
+
+
+def fetch_patch_release_notes(hostname, version):
+    url = f"{hostname}/v1/patchReleaseNotes/{version}"
+    response = requests.get(url)
+    response.raise_for_status()  # Will raise an error for bad responses
+    return response.json()
+
+
+def generate_formatted_output(data):
+    output = ['The following packages have been upgraded, to address the mentioned CVEs:']
+    for package in data["packageList"]:
+        upgrade_line = (
+            f"- upgrade '{package['sourcePackageName']}' from `{package['oldVersion']}` "
+            f"to `{package['newVersion']}`"
+        )
+        output.append(upgrade_line)
+
+        if package["fixedCves"]:
+            cve_line = "  - " + ", ".join(package["fixedCves"])
+            output.append(cve_line)
+
+    return "\n".join(output)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Print information about fixed CVEs in Garden Linux patch releases."
+    )
+    parser.add_argument(
+        "--hostname", type=str, required=True, help="The hostname of the API endpoint."
+    )
+    parser.add_argument(
+        "--version",
+        type=str,
+        required=True,
+        help="The version number for the patch release notes.",
+    )
+
+    args = parser.parse_args()
+
+    data = fetch_patch_release_notes(args.hostname, args.version)
+    formatted_output = generate_formatted_output(data)
+    print(formatted_output)
+
+
+if __name__ == "__main__":
+    main()
