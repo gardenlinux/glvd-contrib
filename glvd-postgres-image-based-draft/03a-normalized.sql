c66a1f82f806c83930febd1b69a759654f034ddb
Florian Wilhelm <florian.wilhelm02@sap.com>
2025-12-08 15:58:05 +0100

    update

--- glvd-postgres-image-based-draft/03a-normalized.sql
+++ glvd-postgres-image-based-draft/03a-normalized.sql
@@ -1,8 +1,10 @@
 -- canonical normalized table
 CREATE TABLE image_variantA (
   id bigserial PRIMARY KEY,
-  cname text NOT NULL UNIQUE,
-  metadata jsonb DEFAULT '{}'    -- optional
+  image_name text NOT NULL,         -- renamed from cname
+  image_version text NOT NULL,            -- new field for version
+  commit_id text,                   -- new field for commit id
+  metadata jsonb DEFAULT '{}'       -- optional
 );
 
 CREATE TABLE image_package (
@@ -18,9 +20,10 @@ CREATE INDEX idx_image_package_image ON image_package (image_variantA_id);
 ALTER TABLE image_variantA ADD COLUMN packages text[] DEFAULT '{}'::text[];
 CREATE INDEX idx_image_variantA_packages_gin ON image_variantA USING GIN (packages);
 
-INSERT INTO image_variantA (cname, packages) VALUES
-  ('gcp-gardener_prod-amd64-1592.4-deadbeef', ARRAY['python3.12', 'rsync', 'jinja2']),
-  ('aws-gardener_prod-amd64-1592.4-beefdead', ARRAY['python3.13', 'python3.12', 'rsync']);
+-- Insert data with separated version and commit_id, and image_name without version/commit
+INSERT INTO image_variantA (image_name, image_version, commit_id, packages) VALUES
+  ('gcp-gardener_prod', '1592.4', 'deadbeef', ARRAY['python3.12', 'rsync', 'jinja2']),
+  ('aws-gardener_prod', '1592.4', 'beefdead', ARRAY['python3.13', 'python3.12', 'rsync']);
 
 INSERT INTO image_package (image_variantA_id, package_name)
 SELECT id, unnest(packages)
@@ -35,7 +38,10 @@ SELECT DISTINCT
   dc.deb_version  AS source_package_version,
   dpc.cpe_version AS gardenlinux_version,
   ((dc.debsec_vulnerable AND (cc.is_resolved IS NOT TRUE)) = true) AS is_vulnerable,
-  dc.debsec_vulnerable
+  dc.debsec_vulnerable,
+  iv.image_name,
+  iv.image_version,
+  iv.commit_id
 FROM public.all_cve     ac
 JOIN public.deb_cve     dc USING (cve_id)
 JOIN public.dist_cpe    dpc ON dc.dist_id = dpc.id
@@ -43,8 +49,9 @@ LEFT JOIN public.cve_context cc USING (cve_id, dist_id)
 -- join to image_package -> image_variantA to restrict to packages in the image
 JOIN public.image_package ip ON ip.package_name = dc.deb_source
 JOIN public.image_variantA   iv ON iv.id = ip.image_variantA_id
+-- join dpc.cpe_version on iv.image_version
 WHERE 
-  -- iv.cname = $1 AND
   dpc.cpe_product = 'gardenlinux'
   AND dc.debsec_vulnerable = true
-  AND dc.deb_source <> 'linux';
+  AND dc.deb_source <> 'linux'
+  AND dpc.cpe_version = iv.image_version;
